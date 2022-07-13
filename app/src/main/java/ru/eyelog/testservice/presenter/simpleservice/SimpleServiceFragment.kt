f8f1b82dee49c09f184a0bd260d971ea4552b2b1
Лисивенко Михаил Алексеевич <mlisivenko@alfabank.ru>
2022-07-13 19:18:37 +0300

    Refactored simple service

--- app/src/main/java/ru/eyelog/testservice/presenter/simpleservice/SimpleServiceFragment.kt
+++ app/src/main/java/ru/eyelog/testservice/presenter/simpleservice/SimpleServiceFragment.kt
@@ -1,15 +1,24 @@
 package ru.eyelog.testservice.presenter.simpleservice
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_service.buttonStart
+import kotlinx.android.synthetic.main.fragment_service.buttonStop
+import kotlinx.android.synthetic.main.fragment_service.textView
 import ru.eyelog.testservice.R
+import ru.eyelog.testservice.services.CustomService
 
 @AndroidEntryPoint
-class SimpleServiceFragment: Fragment() {
+class SimpleServiceFragment: Fragment(), LifecycleOwner {
+
+    private val customService = CustomService()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,5 +26,17 @@ class SimpleServiceFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_service, container, false)
+
+        customService.lifeContainer.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+
+        buttonStart.setOnClickListener {
+            customService.startService(Intent(requireContext(), CustomService::class.java))
+        }
+
+        buttonStop.setOnClickListener {
+            customService.stopSelf()
+        }
     }
 }
--- app/src/main/java/ru/eyelog/testservice/services/CustomService.kt
+++ app/src/main/java/ru/eyelog/testservice/services/CustomService.kt
@@ -3,14 +3,44 @@ package ru.eyelog.testservice.services
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import java.util.concurrent.TimeUnit
 
 class CustomService: Service() {
+
+    lateinit var lifeContainer: MutableLiveData<String>
+
+    private var serviceDisposable: Disposable? = null
+
+    private val observable = Observable.interval(100L, TimeUnit.MILLISECONDS)
+
+    override fun onCreate() {
+        super.onCreate()
+        lifeContainer = MutableLiveData()
+    }
 
     override fun onBind(p0: Intent?): IBinder? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
+        Log.i("Logcat", "Service started")
+        serviceDisposable = observable
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                lifeContainer.postValue("some post")
+            }
 
         return START_STICKY
+    }
+
+    override fun onDestroy() {
+        Log.i("Logcat", "Service destroyed")
+
+        serviceDisposable?.dispose()
+        super.onDestroy()
     }
 }
