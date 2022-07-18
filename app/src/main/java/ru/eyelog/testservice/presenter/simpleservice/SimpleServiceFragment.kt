56fe8d990591bf4c3000e333a56a030093100b55
eyelog <41166734+eyelog@users.noreply.github.com>
2022-07-18 12:37:31 +0300

    Service binder applied

--- app/src/main/java/ru/eyelog/testservice/presenter/simpleservice/SimpleServiceFragment.kt
+++ app/src/main/java/ru/eyelog/testservice/presenter/simpleservice/SimpleServiceFragment.kt
@@ -1,7 +1,10 @@
 package ru.eyelog.testservice.presenter.simpleservice
 
-import android.content.Intent
+import android.content.*
+import android.content.Context.BIND_AUTO_CREATE
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,8 +14,16 @@ import kotlinx.android.synthetic.main.fragment_service.*
 import ru.eyelog.testservice.R
 import ru.eyelog.testservice.services.CustomService
 
+private const val BROADCAST_ACTION = "ru.eyelog.testservice.presenter.simpleservice"
+
 @AndroidEntryPoint
 class SimpleServiceFragment: Fragment() {
+
+    private var serviceBound = false
+
+    private val tickReceiver by lazy { makeBroadcastReceiver() }
+    lateinit var serviceConnection: ServiceConnection
+    lateinit var customService: CustomService
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,18 +36,63 @@ class SimpleServiceFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-
         buttonStart.setOnClickListener {
             requireContext().startService(Intent(requireContext(), CustomService::class.java))
         }
 
         buttonStop.setOnClickListener {
+            requireContext().unbindService(serviceConnection)
             requireContext().stopService(Intent(requireContext(), CustomService::class.java))
         }
+
+        btToggleService.setOnClickListener {
+            customService.setMessageCounter(100)
+        }
+
+        val intentFilter = IntentFilter(BROADCAST_ACTION)
+        requireContext().registerReceiver(tickReceiver, intentFilter)
+
+        val binderIntent = Intent(requireContext(), CustomService::class.java)
+        serviceConnection = object : ServiceConnection{
+            override fun onServiceConnected(p0: ComponentName?, service: IBinder?) {
+                val binder = service as CustomService.CustomBinder
+                customService = binder.getService()
+                serviceBound = true
+            }
+
+            override fun onServiceDisconnected(p0: ComponentName?) {
+                serviceBound = false
+            }
+        }
+
+        requireContext().bindService(binderIntent, serviceConnection, BIND_AUTO_CREATE)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        try {
+            requireContext().unregisterReceiver(tickReceiver)
+        } catch (e: IllegalArgumentException) {
+            Log.e("Broadcast", "Time tick Receiver not registered", e)
+        }
+        requireContext().unbindService(serviceConnection)
+        requireContext().stopService(Intent(requireContext(), CustomService::class.java))
     }
 
     private fun setMessage(value: String){
         tvTitle.text = value
+    }
+
+    private fun makeBroadcastReceiver(): BroadcastReceiver {
+        return object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Log.i("Logcat", "Service message received $intent")
+                intent?.let {
+                    intent.getStringExtra("EXTRA")?.let {
+                        setMessage(it)
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/ru/eyelog/testservice/services/CustomService.kt
+++ app/src/main/java/ru/eyelog/testservice/services/CustomService.kt
@@ -2,27 +2,70 @@ package ru.eyelog.testservice.services
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import java.util.concurrent.TimeUnit
 
 class CustomService: Service() {
+
+    private val customBinder = CustomBinder()
+    private var messageDisposable: Disposable? = null
+    private var messageCounter = 0
 
     override fun onCreate() {
         Log.i("Logcat", "Service onCreate")
         super.onCreate()
     }
 
-    override fun onBind(p0: Intent?): IBinder? = null
+    override fun onBind(p0: Intent?): IBinder {
+        Log.i("Logcat", "Service onBind")
+        return customBinder
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.i("Logcat", "Service started")
 
+        setCustomMessage("Start")
+
+        messageDisposable = Observable.interval(1000L, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                messageCounter++
+                setCustomMessage("Message $messageCounter")
+            }
+
         return START_STICKY
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Log.i("Logcat", "Service onUnbind")
+        messageDisposable?.dispose()
+        return super.onUnbind(intent)
+    }
+
+    private fun setCustomMessage(value: String){
+        val sendIntent = Intent()
+        sendIntent.action = "ru.eyelog.testservice.presenter.simpleservice"
+        sendIntent.putExtra("EXTRA", "Some $value message from service");
+        sendBroadcast(sendIntent)
+    }
+
+    fun setMessageCounter(value: Int) {
+        Log.i("Logcat", "setCustomMessage")
+        messageCounter = value
     }
 
     override fun onDestroy() {
         Log.i("Logcat", "Service destroyed")
-
+        messageDisposable?.dispose()
         super.onDestroy()
+    }
+
+    inner class CustomBinder: Binder(){
+        fun getService(): CustomService = this@CustomService
     }
 }
